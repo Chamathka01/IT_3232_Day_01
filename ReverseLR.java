5d30baa7423a563ad47c55d9f5468c6b15099d90
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-03-14 15:35:23 +0000

    Define the Class, Main Method and take user input.

--- ReverseLR.java
+++ ReverseLR.java
@@ -7,4 +7,12 @@ input 13
 output 6 5 4 3 2 1 7 13 12 11 10 9 8*/
 
 import java.util.Scanner;
+public class ReverseLR {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt();
+
+
+    }
+}
 
