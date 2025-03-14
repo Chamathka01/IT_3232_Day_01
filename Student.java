a350abe8e9424cf022e022ee47100b59b47858bc
Chamathka01 <139635624+Chamathka01@users.noreply.github.com>
2025-03-14 16:17:22 +0000

    Constructor for Student

--- Student.java
+++ Student.java
@@ -7,4 +7,9 @@ import java.util.ArrayList;
 class Student {
     private String name;
     private int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
 }
